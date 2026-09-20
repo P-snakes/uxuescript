2081af2c3645648c93744cceedebbf4231b934f2
unraous <unraous@qq.com>
2026-09-20 14:20:51 +0800

    chore: sync release version to 2.1.3

--- src-tauri/src/config/metadata.rs
+++ src-tauri/src/config/metadata.rs
@@ -20,7 +20,7 @@ impl Default for MetadataConfig {
         Self {
             author: "unraous".into(),
             title: "uXueScript".into(),
-            version: "2.1.2".into(),
+            version: "2.1.3".into(),
             home_url: Url::parse("https://i.mooc.chaoxing.com/").expect("Invalid home URL"),
             log_level: if cfg!(debug_assertions) {
                 LevelFilter::Debug
